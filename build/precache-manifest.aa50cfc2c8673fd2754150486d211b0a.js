a9205d724ce49a3eb29550021878450aa33cd79f
Nirbhay897e <guptanirbhay897@gmail.coml>
2022-12-27 15:54:15 +0530

    first

---
+++ build/precache-manifest.aa50cfc2c8673fd2754150486d211b0a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921aed982e0bc26d5e014279f5c0bbdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28590cc89be37effedf3",
+    "url": "/static/js/2.e191c4e3.chunk.js"
+  },
+  {
+    "revision": "c1065a19b35797015d96",
+    "url": "/static/js/main.4d9df57f.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
